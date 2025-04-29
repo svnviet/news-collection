2a3ea1cba736f00b8dc4067d91a022182cc21122
Vin <svnviet@gmail.com>
2025-04-30 02:37:06 +0700

    save

--- services.py
+++ services.py
@@ -8,9 +8,10 @@ def get_vn_express(rss_url):
     soup = BeautifulSoup(response.content, "xml")
 
     data = []
-
+    next_is_long = 10
+    idx_is_long = 0
     # Loop through each item
-    for item in soup.find_all("item"):
+    for idx, item in enumerate(soup.find_all("item")):
         title = item.title.text
         link = item.link.text
         description = item.description.text
@@ -20,12 +21,19 @@ def get_vn_express(rss_url):
         img_tag = desc_soup.find("img")
         image_url = img_tag["src"] if img_tag else None
 
+        is_long = False
+        if len(title) > 10:
+            if idx_is_long < (idx - next_is_long):
+                is_long = True
+                idx_is_long = idx
+
         data.append(
             {
                 "title": title,
                 "link": link,
                 "image_url": image_url,
-                "source_logo_url": "logo/vne_logo_rss.png"
+                "source_logo_url": "logo/vne_logo_rss.png",
+                "is_long": is_long,
             }
         )
 
