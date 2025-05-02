7c89cc4c0aad33cccae355df1f47f6320e1a926e
Vin <svnviet@gmail.com>
2025-05-03 03:10:28 +0700

    save

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
-from services import get_vn_express
+from services import get_vn_express, get_article_detail
 
 app = Flask(__name__)
 
@@ -27,15 +27,56 @@ def get_data():
 @app.route("/", methods=["GET"])
 def get_news():
     hot_news = get_vn_express("https://vnexpress.net/rss/thoi-su.rss", is_slide=True)
-    items = get_vn_express("https://vnexpress.net/rss/tin-moi-nhat.rss")
-
-    hot_news = [new for new in hot_news if new["image_url"]][:15]
+    items = get_vn_express("https://vnexpress.net/rss/thoi-su.rss")[9:]
+    category = request.args.get("the-loai")
+    if category == "the-gioi":
+        hot_news = get_vn_express("https://vnexpress.net/rss/the-gioi.rss", is_slide=True)
+        items = get_vn_express("https://vnexpress.net/rss/the-gioi.rss")[9:]
 
     return render_template(
         "index.html",
         hot_news=hot_news,
         items=items,
     )
+
+@app.route("/<path:theloai>", methods=["GET"])
+def get_news_by(theloai):
+    hot_news = get_vn_express("https://vnexpress.net/rss/the-gioi.rss", is_slide=True)
+    items = get_vn_express("https://vnexpress.net/rss/the-gioi.rss")[9:]
+
+    if theloai == "the-gioi":
+        hot_news = get_vn_express("https://vnexpress.net/rss/the-gioi.rss", is_slide=True)
+        items = get_vn_express("https://vnexpress.net/rss/the-gioi.rss")[9:]
+
+    return render_template(
+        "index.html",
+        hot_news=hot_news,
+        items=items,
+    )
+
+
+
+@app.route("/load_more", methods=["GET"])
+def load_more_news():
+    page = int(request.args.get("page", 1))
+    per_page = 4
+    all_items = get_vn_express("https://vnexpress.net/rss/tin-noi-bat.rss")
+    items = all_items[(page - 1) * per_page: page * per_page]
+
+    return jsonify(items)
+
+
+@app.route('/vn-vi/news/<path:slug>', methods=['GET'])
+def detail(slug):
+    link = "https://vnexpress.net/" + slug
+    if not link:
+        return "Missing link", 400
+    # Parse the article detail (you'll need a parser here)
+    detail_data = get_article_detail(link)
+    detail_data["article_url"] = link
+
+    item = get_vn_express("https://vnexpress.net/rss/tin-moi-nhat.rss")[1]
+    return render_template("detail.html", article=detail_data, item=item)
 
 
 if __name__ == "__main__":
--- services.py
+++ services.py
@@ -1,6 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
+vn_url = "https://vnexpress.net/"
+base_url = "http://127.0.0.1:5000/"
+detail_url = "http://127.0.0.1:5000/vn-vi/news/"
+
+
+def is_media_content(article_url):
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    soup = soup.find_all("div", {"class": "media_content"})
+    return True if len(soup) > 0 else False
+
+
+def get_rss_list():
+    response = requests.get(vn_url + "rss")
+    soup = BeautifulSoup(response.content, "html.parser")
+    rss_wrap = soup.find("div", {"class": "wrap-list-rss"})
+    rss_list = [rss_data.get("href") for rss_data in rss_wrap.find_all("a")]
+    return rss_list
+
 
 def get_vn_express(rss_url, is_slide=False):
     # Load RSS feed
@@ -8,12 +27,14 @@ def get_vn_express(rss_url, is_slide=False):
     soup = BeautifulSoup(response.content, "xml")
 
     data = []
-    next_is_long = 10
+    slide_count = 0
+    max_slide_count = 10
     idx_is_long = 0
+    next_is_long = 10
     # Loop through each item
     for idx, item in enumerate(soup.find_all("item")):
         title = item.title.text
-        link = item.link.text
+        link = item.link.text.replace(vn_url, "")
         description = item.description.text
 
         # Extract image URL from description using BeautifulSoup again (HTML inside CDATA)
@@ -22,7 +43,7 @@ def get_vn_express(rss_url, is_slide=False):
         image_url = img_tag["src"] if img_tag else None
 
         is_long = False
-        if len(title) > 10:
+        if len(title) > 50:
             if idx_is_long < (idx - next_is_long):
                 is_long = True
                 idx_is_long = idx
@@ -37,6 +58,206 @@ def get_vn_express(rss_url, is_slide=False):
         if is_slide == False:
             row["is_long"] = is_long
 
+        if is_slide:
+            if not image_url:
+                continue
+            slide_count += 1
+            if slide_count > max_slide_count:
+                break
+
         data.append(row)
 
     return data
+
+
+def get_article_detail(link):
+    resp = requests.get(link)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+
+    title = soup.find("h1").get_text(strip=True)
+    article_end = soup.find('span', id='article-end')
+    if article_end is None:
+        author = None
+    else:
+        p_tag = article_end.find_previous_sibling('p')
+        author = p_tag.decode_contents() if p_tag else None
+
+    description = soup.find("p", class_="description")
+    content_div = soup.select_one(".fck_detail")
+
+    slide_show_tag = soup.find_all("div", class_="item_slide_show")
+    video_show_tag = soup.find_all("div", class_="wrap_video")
+    for slide in slide_show_tag:
+        slide.decompose()
+    for video in video_show_tag:
+        video.decompose()
+
+    for tag in content_div.find_all(True):  # True means find all tags
+        if tag.name == 'figure':
+            try:
+                new_tag = make_picture(soup, tag)
+                tag.insert_before(new_tag)
+                tag.decompose()  # Removes the tag from the tree
+            except Exception as e:
+                tag.decompose()
+        if tag.name == 'a':
+            tag['target'] = "_blank"
+            tag['href'] = tag['href'].replace(vn_url, detail_url)
+
+    content_html = str(content_div) if content_div else ""
+
+    published_at_tag = soup.find("span", class_="date")
+    published_at = published_at_tag.text.strip() if published_at_tag else ""
+
+    return {
+        "title": title,
+        "author": author,
+        "description": description,
+        "content": content_html,
+        "published_at": published_at,
+    }
+
+
+def get_element_text(element):
+    try:
+        return element.get_text(strip=True)
+    except AttributeError:
+        return ""
+
+
+def make_picture(soup, tag):
+    # Create <figure>
+    figure = soup.new_tag('figure', attrs={
+        'data-size': 'true',
+        'itemprop': 'associatedMedia image',
+        'itemscope': '',
+        'itemtype': 'http://schema.org/ImageObject',
+        'class': 'tplCaption action_thumb_added'
+    })
+
+    meta_image_url = tag.find('meta', itemprop='url')
+    meta_width = tag.find('meta', itemprop='width')
+    meta_height = tag.find('meta', itemprop='height')
+    meta_caption = tag.find('figcaption', itemprop='description')
+    image_url = meta_image_url['content'].replace("amp;", "") if meta_image_url else ""
+    width = meta_width['content'] if meta_height else 0
+    height = meta_height['content'] if meta_width else 0
+    caption = meta_caption.text if meta_caption else ""
+
+    # Add <meta> tags
+    for prop, val in [
+        ('url', image_url),
+        ('width', width),
+        ('height', height),
+        ('href', '')
+    ]:
+        meta = soup.new_tag('meta', itemprop=prop, content=val)
+        figure.append(meta)
+
+    # fig-picture
+    fig_div = tag.find('div', class_='fig-picture')
+    fig_picture = soup.new_tag('div', attrs={
+        'class': 'fig-picture el_valid',
+        'style': save_get_bs(fig_div, 'style'),
+        'data-src': image_url,
+        'data-sub-html': f'<div class="ss-wrapper"><div class="ss-content"><p class="Image">{caption}</p></div></div>'
+    })
+
+    # <picture> with <source> and <img>
+    picture = soup.new_tag('picture')
+    source = soup.new_tag('source', attrs={
+        'data-srcset': f'{image_url} 1x'
+    })
+    fig_img_src = fig_div.find('img', itemprop="contentUrl")
+    intrinsicsize = fig_img_src['intrinsicsize']
+    fig_img_style = fig_img_src['style']
+
+    img = soup.new_tag('img', attrs={
+        'itemprop': 'contentUrl',
+        'src': image_url,
+        'alt': caption,
+        'class': 'lazy lazied',
+        'loading': 'lazy',
+        'intrinsicsize': intrinsicsize,
+        'style': fig_img_style,
+    })
+    picture.append(source)
+    picture.append(img)
+    fig_picture.append(picture)
+    figure.append(fig_picture)
+
+    # figcaption
+    figcaption = soup.new_tag('figcaption', itemprop='description')
+    p_caption = soup.new_tag('p', attrs={'class': 'Image'})
+    p_caption.string = caption
+    figcaption.append(p_caption)
+    figure.append(figcaption)
+
+    return figure
+
+
+def make_video(soup, tag):
+    # Create <figure>
+    figure = soup.new_tag('figure', attrs={
+        'data-size': 'true',
+        'itemprop': 'associatedMedia video',
+        'itemscope': '',
+        'itemtype': 'http://schema.org/VideoObject',
+        'class': 'tplCaption action_thumb_added'
+    })
+
+    # Locate embed container
+    embed_div = tag.find('div', class_='embed-container')
+    video_parent = tag.find('div', class_='box_embed_video_parent')
+
+    # Get data attributes
+    video_id = video_parent['data-vid'] if video_parent else ''
+    video_url = f'https://example.com/embed/{video_id}' if video_id else ''  # Adjust to your actual embed URL pattern
+    width = video_parent.get('data-width', '100%')
+    height = video_parent.get('data-height', '382.5px')
+
+    # Caption
+    figcaption_div = tag.find('figcaption', class_='desc_cation')
+    caption_p = figcaption_div.find('p', class_='Image') if figcaption_div else None
+    caption = caption_p.get_text(strip=True) if caption_p else ''
+
+    # Add <meta> tags
+    for prop, val in [
+        ('contentUrl', video_url),
+        ('width', width),
+        ('height', height),
+        ('href', '')
+    ]:
+        meta = soup.new_tag('meta', itemprop=prop, content=val)
+        figure.append(meta)
+
+    # fig-video
+    fig_video = soup.new_tag('div', attrs={
+        'class': 'fig-video el_valid',
+        'style': save_get_bs(embed_div, 'style'),
+        'data-src': video_url,
+        'data-sub-html': f'<div class="ss-wrapper"><div class="ss-content"><p class="Video">{caption}</p></div></div>'
+    })
+
+    # Reuse the embed container as-is
+    if embed_div:
+        # Clone the embed container and insert into fig_video
+        fig_video.append(embed_div)
+
+    figure.append(fig_video)
+
+    # figcaption
+    figcaption = soup.new_tag('figcaption', itemprop='description')
+    p_caption = soup.new_tag('p', attrs={'class': 'Video'})
+    p_caption.string = caption
+    figcaption.append(p_caption)
+    figure.append(figcaption)
+
+    return figure
+
+
+def save_get_bs(content, attribute):
+    try:
+        return content.get(attribute)
+    except AttributeError:
+        return ""
